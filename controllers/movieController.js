c84df02ea449ccdfb93e2e50d26d1abd7e291d04
carminenicotera <carmine.91.n@hotmail.it>
2026-04-22 10:35:59 +0200

    add CORS support

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,55 @@
 const connection = require('../database/connection')
 
 const index = (req, res) => {
-  res.json({ message: 'index all movies' })
+
+  const sql = 'SELECT * FROM movies'
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).json({
+        error: 'Database query failed'
+      })
+    }
+    res.json(results)
+  })
 }
 
 const show = (req, res) => {
-  res.json({ message: 'show movie' })
+
+  const sql = 'SELECT * FROM movies WHERE id = ?'
+  const id = Number(req.params.id)
+  const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?'
+
+  // recupero il film con l'id specificato
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).json({
+        error: 'Database query failed'
+      })
+    }
+    if (results.length === 0) {
+      return res.status(404).json({
+        error: 'Movie not found'
+      })
+    }
+
+    // recupero le recensioni associate al film
+    connection.query(reviewsSql, [id], (err, reviewsResults) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).json({
+          error: 'Database query failed'
+        })
+      }
+
+      // aggiungo l'array delle recensioni al film e lo restituisco come risposta JSON al client 
+      results[0].reviews = reviewsResults
+
+      res.json(results[0])
+    })
+  })
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const movieRouter = require('./routes/movies')
 const notFound = require('./middlewares/notFound')
 const serverError = require('./middlewares/serverError')
 
+const cors = require('cors')
+
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
+app.use(cors())
+// oppure solo per la frontend
+// app.use(cors({
+//   origin: process.env.FRONT_ORIGIN || 'http://localhost:3000'
+// }))
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
